fc9d4ee0c5d44c427fd2eae7faf73365d4c0a955
bala-amypo <bala@amypo.in>
2025-12-17 05:28:12 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -8,5 +8,5 @@ public interface UserService{
     public User registerUser(User user);
     public User getUser(long id);
     public List<User> getAllUsers();
-    
+    public User getByEmail(String email);
 }
--- src/main/java/com/example/demo/service/implementation/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/implementation/UserServiceImpl.java
@@ -1,8 +1,40 @@
 package com.example.demo.service.Implementation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
+    List<User> list=new ArrayList<>();
+
+    public User registerUser(User user){
+        list.add(user);
+        return user;
+    }
     
+    public User getUser(long id){
+        for(User li:list){
+            if(li.getId()==id){
+                return li;
+            }
+        }
+        return null;
+    }
+
+    public List<User> getAllUsers(){
+        return list;
+    }
+    public User getByEmail(String email){
+        for(User li:list){
+            if(li.getEmail()==email){
+                return li;
+            }
+        }
+        return null;
+    }
 }
