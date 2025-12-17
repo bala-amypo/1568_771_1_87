e8b3fa51916a1b9dacf7cbdf87a687900d5f93de
bala-amypo <bala@amypo.in>
2025-12-17 05:37:47 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.UserService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> registerUser(@RequestBody User user){
+        User u = userService.registerUser(user);
+        return ResponseEntity.status(201).body(u);
+    }
 }
